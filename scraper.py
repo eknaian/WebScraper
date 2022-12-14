fe25425f611caaf6f1ab4ea4f4fde88fa5513439
eknaian <eknaian@gmail.com>
2022-12-14 11:09:19 -0500

    file returns the first 2 pages of hackernews sorted by the highest number of points. Only shows results over 100. Only displays title, link, and number of points. Built using the BeautifulSoup library

--- scraper.py
+++ scraper.py
@@ -1,6 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
+# heads up! .storylink changed to .titleline
+links = soup.select('.titleline > a')
+subtext = soup.select('.subtext')
+# heads up! .storylink changed to .titleline
+links2 = soup2.select('.titleline > a')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
