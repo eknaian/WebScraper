be7c52adc5da8eaacd65a0c9447a940edcfac333
eknaian <eknaian@gmail.com>
2022-12-14 11:10:48 -0500

    minor changes

--- scraper.py
+++ scraper.py
@@ -7,10 +7,10 @@ res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
 soup2 = BeautifulSoup(res2.text, 'html.parser')
 
-# heads up! .storylink changed to .titleline
+
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
-# heads up! .storylink changed to .titleline
+
 links2 = soup2.select('.titleline > a')
 subtext2 = soup2.select('.subtext')
 
